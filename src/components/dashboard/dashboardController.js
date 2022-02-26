741a6825cb11e76869cad67b7b3148360c313c76
devEdu-web <santos.edu3301@gmail.com>
2022-02-25 22:00:12 -0300

    user picture displaying on dashboard and the mobile menu

--- src/components/dashboard/dashboardController.js
+++ src/components/dashboard/dashboardController.js
@@ -1,12 +1,14 @@
 import {getClassesPercentage} from '../../util/getPercentage.js'
 import {getHoursPercentage} from '../../util/getPercentage.js'
 import {UserProgress} from '../users/UserProgress.js'
+import {User} from '../users/User.js'
 
 async function getDashboardPage(req, res, next) {
     const userId = req.cookies.userId
     const userName = req.cookies.userName
     const words = await getClassesPercentage(userId)
     const hours = await getHoursPercentage(userId)
+    const userInfo = await User.findUserById(userId)
     const userProgress = await UserProgress.getUserProgress(userId)
 
     console.log(words)
@@ -16,7 +18,8 @@ async function getDashboardPage(req, res, next) {
         userName,
         userProgress,
         words,
-        hours
+        hours,
+        userInfo
     })
 
 }
