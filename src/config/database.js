d7e0d8ae9c8ae92a74ec792171fdcf926beac4b2
devEdu-web <santos.edu3301@gmail.com>
2022-03-10 21:06:03 -0300

    database added to git repository

---
+++ src/config/database.js
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+const URI =
+    `mongodb+srv://eduardo:${process.env.MONGO_CLUSTER_PASSWORD}@cluster0.hscoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const client = new MongoClient(URI);
+let db;
+
+async function connectingToDatabase() {
+    try {
+        await client.connect();
+        db = client.db("english-management") || [];
+        // console.log(db);
+        console.log("Connected successfuly");
+        
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function getDb() {
+    if (db) {
+        return db;
+    } else {
+        return [];
+    }
+}
+
+export { connectingToDatabase, db };
